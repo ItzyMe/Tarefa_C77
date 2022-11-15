68dd740d79bd3a6b34a2b8a999c6cba7c308ab72
ItzyMe <118301481+ItzyMe@users.noreply.github.com>
2022-11-15 15:05:18 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+//Concatenar
+var nome="Ranbit"
+var sobrenome="Kapoor"
+var nomeCompleto= nome.concat(sobrenome);
+console.log (nomeCompleto);
+//TODO TEXTO NA PROGRAMAÇÃO VEM ENTRE ASPAS
+ var a="10";
+ var b= "5";
+ console.log (resultado);
+ var soma= a+b
+ console.log(soma);
+ var resultado=a.concat(b); //Juntar as variáveis A e B como um texto, que vai imprimir o número 105
+
+ var c=10;
+ var d=5;
+ var somaDois=c+d; 
+ console.log(somaDois);
+ //var Concatenar= c.concat(d);
+ //console.log(Concatenar);
+ var multiplicar= c*d
+ console.log(multiplicar);
+ var divisão= c/d;
+ console.log(divisão);
